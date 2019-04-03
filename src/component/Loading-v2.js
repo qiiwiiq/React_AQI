3c7b9e28cac29cfc16500675bb799c6e94d14b33
PTT <qiiwiiq@hotmail.com>
2019-04-04 00:07:11 +0800

    Change loading page icon

--- src/component/Loading-v2.js
+++ src/component/Loading-v2.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import '../css/Loading-v2.css';
-import '../css/icon-font.css';
 
 const Loading = () => {
     return (
         <div className="loading-page">
             <div className="loading-page-container">
-                <i className="icon-weather-wind-sun"></i>
+                <i className="fas fa-cloud-sun"></i>
                 <div className="cover"></div>
             </div>
         </div>
